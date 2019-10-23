7c2c02dd1edea3f06219354c289c40bb99ff8324
Gustavo Patrocínio <gustavopatrocinio00@gmail.com>
2019-10-23 14:06:26 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -12,5 +12,14 @@ public class Utils {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
 	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
+	}
+	
 	
 }
